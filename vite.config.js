5b2f88c0b10e8c80260ad476f29b13e5102e637a
rabbaniST <golamrabbanig5@gmail.com>
2024-12-02 21:35:02 +0600

    bs5 setup for design

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path')
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  '~bootstrap': path.resolve(__dirname, './node_modules/bootstrap'),
 })
